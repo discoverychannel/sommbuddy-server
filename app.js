f21613aef675785434679e375f04aa3bd02c163e
Spencer Scarpelli <spencerscarpelli@gmail.com>
2016-06-14 13:42:47 -0700

    start on post route, read request object

--- app.js
+++ app.js
@@ -4,15 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var rp = require('request-promise');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+// var routes = require('./routes/index');
+// var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +23,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+// app.use('/', routes);
+// app.use('/users', users);
+
+app.post('/', (req, res, next) {
+  var type = req.body.type; //red, white, bubbles, etc.
+  var wineFlavor = req.body.wineFlavor; //sweet, dry, exotic, etc.
+  var profile = req.body.profile; //herbacious, piney, oakey, etc.
+  var mealWeight = req.body.mealWeight; //light, heavy; entree, appetizers, dessert, etc.
+  var foodFlavor = req.body.foodFlavor; //savory, sweet, etc.
+  var price = req.body.price; //$, $$, $$$, etc.
+  var options = {
+    method = 'GET',
+    uri: 'http://services.wine.com/api/beta2/service.svc/format/resource?apikey=key&parameters'
+  }
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
